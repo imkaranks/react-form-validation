ad9b7fadaba65ea11c2d35e2b898f8794fb61774
Karan <risingphoenix160@gmail.com>
2023-05-29 23:41:00 +0530

    style: minor changes in icons and success popup

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -57,10 +57,13 @@ function Form() {
     {
       success
       ? (
-        <section>
-          <h1>Success!</h1>
+        <section className='w-11/12 max-w-lg mx-auto flex flex-col gap-1 items-center bg-white p-8 rounded-md'>
+          <div className='bg-green-600 inline-flex justify-center items-center text-2xl text-white w-12 h-12 rounded-full' aria-hidden='true'>
+            <FontAwesomeIcon icon={faCheck} />
+          </div>
+          <h1 className='text-3xl font-bold text-center text-green-600'>Success!</h1>
           <p>
-            <a href="#">Sign In</a>
+            <a href="#" className='bg-gradient-to-r from-blue-500 to-purple-600 text-lg px-6 py-2 inline-flex mt-3 text-white font-semibold rounded-md cursor-pointer'>Sign In</a>
           </p>
         </section>
       )
@@ -74,12 +77,12 @@ function Form() {
           <h1 className='text-3xl font-bold text-center bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text' style={{WebkitTextFillColor: 'transparent'}}>Register</h1>
           <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div className="flex flex-col gap-1">
-              <label htmlFor="username" className='font-semibold'>
+              <label htmlFor="username" className='flex gap-1 items-center font-semibold'>
                 Username
-                <span className={validName ? 'text-green-600' : 'hidden'}>
+                <span className={validName ? 'bg-green-600 inline-flex justify-center items-center text-xs text-white w-5 h-5 rounded-full' : 'hidden'}>
                   <FontAwesomeIcon icon={faCheck} />
                 </span>
-                <span className={validName || !user ? 'hidden' : 'text-red-600'}>
+                <span className={validName || !user ? 'hidden' : 'bg-red-600 inline-flex justify-center items-center text-xs text-white w-5 h-5 rounded-full'}>
                   <FontAwesomeIcon icon={faTimes} />
                 </span>
               </label>
@@ -94,10 +97,10 @@ function Form() {
                 aria-describedby='uidnote'
                 onFocus={() => setUserFocus(true)}
                 onBlur={() => setUserFocus(false)}
-                className='border border-gray-400 rounded-md px-4 py-1'
+                className='border border-gray-400 rounded-md px-4 py-2'
               />
               <p id="uidnote" className={`${userFocus && user && !validName ? 'relative bg-gray-300 p-2 mt-2 rounded-md before:content-[""] before:absolute before:-top-1 before:left-4 before:w-2 before:h-2 before:rotate-45 before:bg-gray-300' : 'sr-only'}`}>
-                <FontAwesomeIcon icon={faInfoCircle} />
+                <FontAwesomeIcon icon={faInfoCircle} style={{marginRight: '.5rem'}} />
                 4 to 24 characters.<br />
                 Must begin with a letter.<br />
                 Letters, numbers, underscores, hyphens allowed.
@@ -105,12 +108,12 @@ function Form() {
             </div>
 
             <div className="flex flex-col gap-1">
-              <label htmlFor="password" className='font-semibold'>
+              <label htmlFor="password" className='flex gap-1 items-center font-semibold'>
                 Password
-                <span className={validPasswd ? 'text-green-600' : 'hidden'}>
+                <span className={validPasswd ? 'bg-green-600 inline-flex justify-center items-center text-xs text-white w-5 h-5 rounded-full' : 'hidden'}>
                   <FontAwesomeIcon icon={faCheck} />
                 </span>
-                <span className={validPasswd || !passwd ? 'hidden' : 'text-red-600'}>
+                <span className={validPasswd || !passwd ? 'hidden' : 'bg-red-600 inline-flex justify-center items-center text-xs text-white w-5 h-5 rounded-full'}>
                   <FontAwesomeIcon icon={faTimes} />
                 </span>
               </label>
@@ -124,13 +127,13 @@ function Form() {
                 aria-describedby='passwdnote'
                 onFocus={() => setPasswdFocus(true)}
                 onBlur={() => setPasswdFocus(false)}
-                className='border border-gray-400 rounded-md px-4 py-1'
+                className='border border-gray-400 rounded-md px-4 py-2'
               />
               <p
                 id="passwdnote"
                 className={passwdFocus && !validPasswd ? 'relative bg-gray-300 p-2 mt-2 rounded-md before:content-[""] before:absolute before:-top-1 before:left-4 before:w-2 before:h-2 before:rotate-45 before:bg-gray-300' : 'sr-only'}
               >
-                <FontAwesomeIcon icon={faInfoCircle} />
+                <FontAwesomeIcon icon={faInfoCircle} style={{marginRight: '.5rem'}} />
                 8 to 24 characters.<br />
                 Must include uppercase letters, a number and a special character.<br />
                 Allowed special characters: 
@@ -143,12 +146,12 @@ function Form() {
             </div>
 
             <div className="flex flex-col gap-1">
-              <label htmlFor="confirm-passwd" className='font-semibold'>
-                Confirm Password:
-                <span className={validMatch && matchPasswd ? 'text-green-600' : 'hidden'}>
+              <label htmlFor="confirm-passwd" className='flex gap-1 items-center font-semibold'>
+                Confirm Password
+                <span className={validMatch && matchPasswd ? 'bg-green-600 inline-flex justify-center items-center text-xs text-white w-5 h-5 rounded-full' : 'hidden'}>
                   <FontAwesomeIcon icon={faCheck} />
                 </span>
-                <span className={validMatch || !matchPasswd ? 'hidden' : 'text-red-600'}>
+                <span className={validMatch || !matchPasswd ? 'hidden' : 'bg-red-600 inline-flex justify-center items-center text-xs text-white w-5 h-5 rounded-full'}>
                   <FontAwesomeIcon icon={faTimes} />
                 </span>
               </label>
@@ -162,19 +165,19 @@ function Form() {
                 aria-describedby='cpasswdnote'
                 onFocus={() => setMatchFocus(true)}
                 onBlur={() => setMatchFocus(false)}
-                className='border border-gray-400 rounded-md px-4 py-1'
+                className='border border-gray-400 rounded-md px-4 py-2'
               />
               <p
                 id="cpasswdnote"
                 className={matchFocus && !matchPasswd ? 'relative bg-gray-300 p-2 mt-2 rounded-md before:content-[""] before:absolute before:-top-1 before:left-4 before:w-2 before:h-2 before:rotate-45 before:bg-gray-300' : 'sr-only'}
               >
-                <FontAwesomeIcon icon={faInfoCircle} />
-                Must match the first password input field.
+                <FontAwesomeIcon icon={faInfoCircle} style={{marginRight: '.5rem'}} />
+                Must match the password input field.
               </p>
             </div>
 
 
-            <button className='bg-gradient-to-r from-blue-500 to-purple-600 px-6 py-2 w-full text-white font-semibold rounded-md' disabled={!validName || !validPasswd || !validMatch ? true : false}>Sign Up</button>
+            <button className='bg-gradient-to-r from-blue-500 to-purple-600 px-6 py-2 w-full text-lg text-white font-semibold rounded-md cursor-pointer' disabled={!validName || !validPasswd || !validMatch ? true : false}>Sign Up</button>
           </form>
           <p className='mt-4 border-t border-t-gray-400 pt-2 text-center font-semibold'>
             Already registered?<br />
